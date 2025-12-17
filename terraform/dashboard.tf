c162fe1c47204ca8005db34297610887bf7ad602
xkhxl <saiakhil6789@gmail.com>
2025-12-17 15:09:01 +0530

    feat(task8): changed dashboard.tf

--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -9,14 +9,35 @@ resource "aws_cloudwatch_dashboard" "ecs_dashboard" {
         y      = 0
         width  = 12
         height = 6
+
         properties = {
-          metrics = [
-            ["AWS/ECS", "CPUUtilization", "ClusterName", aws_ecs_cluster.strapi.name, "ServiceName", aws_ecs_service.strapi.name],
-            ["AWS/ECS", "MemoryUtilization", "ClusterName", aws_ecs_cluster.strapi.name, "ServiceName", aws_ecs_service.strapi.name]
-          ]
-          period = 60
+          region = var.aws_region
+          title  = "ECS CPU & Memory Utilization"
           stat   = "Average"
-          title  = "ECS CPU & Memory"
+          period = 60
+
+          metrics = [
+            [
+              "AWS/ECS",
+              "CPUUtilization",
+              "ClusterName",
+              aws_ecs_cluster.strapi.name,
+              "ServiceName",
+              aws_ecs_service.strapi.name
+            ],
+            [
+              "AWS/ECS",
+              "MemoryUtilization",
+              "ClusterName",
+              aws_ecs_cluster.strapi.name,
+              "ServiceName",
+              aws_ecs_service.strapi.name
+            ]
+          ]
+
+          annotations = {
+            horizontal = []
+          }
         }
       }
     ]
